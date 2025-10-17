e44580d6b67e77cbbbbe520dc2134e1ffe751967
Amy Opas <amy.opas@proton.me>
2025-10-17 15:50:06 +0200

    add max branch count and color per rarity

--- branch_game/ezterm.py
+++ branch_game/ezterm.py
@@ -1,29 +1,53 @@
 from dataclasses import dataclass, field
 from blessed import Terminal
+from numpy.typing import NDArray
 from branch_game.screen_buffer import Screen, ScreenBuffer
+import numpy as np
 
-RGB = tuple[int, int, int]
+
+@dataclass
+class RGBA:
+    r: float
+    g: float
+    b: float
+    a: float
+
+    def __getitem__(self, key: int | slice) -> float | tuple[float, ...]:
+        components = (self.r, self.g, self.b, self.a)
+        return components[key]
 
 
 @dataclass
 class RichText:
     text: str
-    color: RGB = field(default=(255, 255, 255))
-    bg: RGB | None = None
+    color: RGBA = field(default_factory=lambda: RGBA(1.0, 1.0, 1.0, 1.0))
+    bg: RGBA | None = None
 
 
-def _make_style(term: Terminal, fg: RGB, bg: RGB | None) -> str:
+def _make_style(term: Terminal, fg: RGBA, bg: RGBA | None) -> str:
     if not term.does_styling:
         style = ""
     else:
-        fg_str = term.color_rgb(*fg)
-        bg_str = term.on_color_rgb(*bg) if bg else ""
+        fg_str = term.color_rgb(*_rgba_to_rgb_int(fg))
+        bg_str = term.on_color_rgb(*_rgba_to_rgb_int(bg)) if bg else ""
         style = fg_str + bg_str
     return style
 
 
-def print_at(term: Terminal, screen: Screen, x: int, y: int, text: RichText | list[RichText]) -> None:
-    """ Draws rich text into the screen buffer at (x, y). Each character is styled individually. """
+def _rgba_to_rgb_int(col_rgba: RGBA) -> tuple[int, int, int]:
+    """This returns RGB in ints because blessed uses ints."""
+    col_rgb = col_rgba[:3]
+    alpha = col_rgba[3]
+
+    col_rgb_scaled: NDArray[np.float64] = np.array(col_rgb) * alpha
+    col_rgb_int: NDArray[np.int32] = np.clip(np.round(col_rgb_scaled * 255).astype(int), 0, 255)
+    return tuple(col_rgb_int)
+
+
+def print_at(
+    term: Terminal, screen: Screen, x: int, y: int, text: RichText | list[RichText]
+) -> None:
+    """Draws rich text into the screen buffer at (x, y). Each character is styled individually."""
     buffer: ScreenBuffer = screen.new_buffer
 
     # Normalize text to list in case of RichText for simplicity
--- branch_game/main.py
+++ branch_game/main.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
+from copy import copy
 from dataclasses import dataclass, field
 from enum import Enum, auto
 from functools import partial
+from random import choice, random
 from typing import Callable
 from blessed import Terminal
 from branch_game.screen_buffer import Screen, buffer_diff, flush_diffs
-from branch_game.ezterm import RichText
+from branch_game.ezterm import RGBA, RichText
 from branch_game.fps_limiter import create_fps_limiter
 import branch_game.ezterm as ezterm
 
@@ -31,6 +33,19 @@ class NodeRarity(Enum):
     COMMON = auto()
     UNCOMMON = auto()
     RARE = auto()
+
+
+NODE_RARITY_COLOR = {
+    NodeRarity.COMMON: RGBA(1.0, 1.0, 1.0, 1.0),
+    NodeRarity.UNCOMMON: RGBA(0.2, 0.8, 0.4, 1.0),
+    NodeRarity.RARE: RGBA(0.85, 0.25, 0.3, 1.0),
+}
+
+NODE_RARITY_MAX_BRANCH_COUNT = {
+    NodeRarity.COMMON: 2,
+    NodeRarity.UNCOMMON: 3,
+    NodeRarity.RARE: 4,
+}
 
 
 @dataclass
@@ -73,11 +88,9 @@ def refresh_tree_view(ctx: AppContext):
 
 
 def get_available_node_branch_slots(node: Node) -> int:
-    # This is a placeholder for the time being
-    # every node will have 2 slots atm
-    placeholder_max_branch_slots = 2
+    branch_slots = NODE_RARITY_MAX_BRANCH_COUNT[node.rarity]
 
-    return max(0, placeholder_max_branch_slots - len(node.children))
+    return max(0, branch_slots - len(node.children))
 
 
 def generate_tree_view(ctx: AppContext) -> list[TreeViewItem]:
@@ -101,7 +114,9 @@ def generate_tree_view(ctx: AppContext) -> list[TreeViewItem]:
     return tree_view
 
 
-def tick(ctx: AppContext, print_at: Callable[[int, int, RichText | list[RichText]], None]) -> AppStatus:
+def tick(
+    ctx: AppContext, print_at: Callable[[int, int, RichText | list[RichText]], None]
+) -> AppStatus:
     key = ctx.terminal.inkey(timeout=0.1)
     if key == "q":
         return AppStatus.EXIT
@@ -133,8 +148,11 @@ def tick(ctx: AppContext, print_at: Callable[[int, int, RichText | list[RichText
             ctx.selected_item_index += 1
         case UIAction.ADD_NODE_DRAFT:
             selected_view_item = ctx.tree_view[ctx.selected_item_index]
+            # TODO: remove this, this is just for testing
+            random_rarity = choice(list(NodeRarity))
+
             ctx.node_draft = NodeDraft(
-                node=Node(NodeRarity.COMMON),
+                node=Node(random_rarity),
                 parent=selected_view_item.node,
                 index=ctx.selected_item_index + 1,
                 depth=selected_view_item.depth + 1,
@@ -144,7 +162,7 @@ def tick(ctx: AppContext, print_at: Callable[[int, int, RichText | list[RichText
             ctx.state = State.NODE_DRAFTING
         case UIAction.CONFIRM_NODE_DRAFT:
             if ctx.node_draft:
-                new_node = Node(NodeRarity.COMMON)
+                new_node = ctx.node_draft.node
 
                 if parent_node := ctx.node_draft.parent:
                     parent_node.children.insert(0, new_node)
@@ -167,20 +185,27 @@ def tick(ctx: AppContext, print_at: Callable[[int, int, RichText | list[RichText
     # < = = | Rendering | = = >
     for index, tree_view_item in enumerate(ctx.tree_view):
         text_segments: list[RichText] = []
-        rich_text = RichText("node", color=(155, 155, 155))
+
+        regular_node_alpha = 0.4
+        rich_text_color = copy(NODE_RARITY_COLOR[tree_view_item.node.rarity])
+        rich_text_color.a *= regular_node_alpha
+
         is_selected: bool = index == ctx.selected_item_index
 
         # Override color for highlighting based on the context
         if is_selected and ctx.state != State.NODE_DRAFTING:
             branch_count = len(tree_view_item.node.children)
-            max_branch_count = 2
-            # TODO: figure out how to do RGBA (probably similar to vec4 in shaders)
-            suffix = RichText(f" ({branch_count}/{max_branch_count})", color=(255, 255, 150))
+            max_branch_count = NODE_RARITY_MAX_BRANCH_COUNT[tree_view_item.node.rarity]
+            suffix = RichText(f" ({branch_count}/{max_branch_count})", RGBA(1.0, 0.8, 0.5, 0.4))
             text_segments.append(suffix)
-            rich_text.color = (255, 255, 150)
+            highlighted_node_alpha = 1.0
+            rich_text_color = copy(NODE_RARITY_COLOR[tree_view_item.node.rarity])
+            rich_text_color.a *= highlighted_node_alpha
+            # rich_text.color = RGBA(1.0, 1.0, 1.0, 1.0)
         if tree_view_item.is_draft:
-            rich_text.color = (75, 120, 155)
+            rich_text_color = RGBA(0.5, 1.0, 1.0, 0.5)
 
+        rich_text = RichText("node", color=rich_text_color)
         text_segments.insert(0, rich_text)
 
         print_at(2 * tree_view_item.depth, index, text_segments)
